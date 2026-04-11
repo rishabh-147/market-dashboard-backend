f60713f3989158130594305b2006d6e425326d97
rishabh-147 <onlinestuff.rishabh@gmail.com>
2026-04-11 23:27:30 +0530

    [DEV] Custom Config For CORS

---
+++ src/main/java/com/rishabh/projects/market_dashboard_backend/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.rishabh.projects.market_dashboard_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                            "http://localhost:3000",
+                            "https://your-frontend.vercel.app"
+                        )
+                        .allowedMethods("*");
+            }
+        };
+    }
+}
